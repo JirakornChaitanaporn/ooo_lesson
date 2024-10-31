a2626bcc4c0e56c3fe4be0f1b43101478d0b1ad4
Jirakorn Chaitanaporn <jirakorn.c@ku.th>
2024-10-31 14:33:43 +0700

    Change proceduaral style to OOP style first draft

--- bank_account.py
+++ bank_account.py
@@ -1,70 +1,68 @@
-account_database = []
+class AccountDB:
+    def __init__(self):
+        self.account_list = []
 
-def create_account(num, type, name, init_balance):
-    index = search_account_db(num)
-    if index == -1:
-        account = {}
-        account["account_number"] = num
-        account["type"] = type
-        account["account_name"] = name
-        account["balance"] = init_balance
-        account_database.append(account)
-    else:
-        print("Account", num, "already exists")
-
-def delete_account(num):
-    index = search_account_db(num)
-    if index != -1:
-        print("Deleting account:", account_database[index]["account_number"])
-        del account_database[index]
-    else:
-        print(num, "invalid account number; nothing to be deleted.")
-
-def search_account_db(num):
-    for i in range(len(account_database)):
-        if account_database[i]["account_number"] == num:
-            return i
-    return -1
-
-def deposit(account_num, amount):
-    index = search_account_db(account_num)
-    if index != -1:
-        print("Depositing", amount, "to", account_database[index]["account_number"])
-        account_database[index]["balance"] += amount
-    else:
-        print(account_num, "invalid account number; no deposit action performed.")
-
-def withdraw(account_num, amount):
-    index = search_account_db(account_num)
-    if index != -1:
-        if account_database[index]["balance"] >= amount:
-            print("Withdrawing", amount, "from", account_database[index]["account_number"])
-            account_database[index]["balance"] -= amount
+    def add_account(self, account):
+        if self.search_account_i(account.accNum) == -1:
+            self.account_list.append(account)
         else:
-            print("withdrawal amount", amount, "exceeds the balance of", account_database[index]["balance"], "for", account_num, "account.")
-    else:
-        print(account_num, "invalid account number; no withdrawal action performed.")
+            print("Account", account.accNum, "already exists")
+            
+    def search_account_i(self, num):
+        for i in range(len(self.account_list)):
+            if self.account_list[i] == num:
+                return i
+        return -1
+    
+    def search_acc_obj(self, accNum):
+        for account in self.account_list:
+            if accNum == account.accNum:
+                return account
+            else:
+                return None
+    
+    def __str__(self):
+        s = ''
+        for account in self.account_list:
+            s += str(account) + ", "
+        return s
+    
+class Account:
+    def __init__(self, accNum, type, name, balance):
+        self.accNum = accNum
+        self.type = type
+        self.name = name
+        self.balance = balance
         
-def show_account(account_num):
-    index = search_account_db(account_num)
-    if index != -1:
-        print("Showing details for", account_database[index]["account_number"])
-        print(account_database[index])
-    else:
-        print(account_num, "invalid account number; nothing to be shown for.")
+    def deposit(self, amount):
+        self.balance += amount
+        
+    def withdraw(self, amount):
+        self.balance -= amount
 
-create_account("0000", "saving", "David Patterson", 1000)
-create_account("0001", "checking", "John Hennessy", 2000)
-create_account("0003", "saving", "Mark Hill", 3000)
-create_account("0004", "saving", "David Wood", 4000)
-create_account("0004", "saving", "David Wood", 4000)
+    def __str__(self):
+        return '{' + str(self.accNum) + ',' + str(self.type) + ',' + str(self.accNum) + ',' + str(self.balance) + '}'
+    
+account_database = AccountDB()
+acc1 = Account("0000", "saving", "David Patterson", 1000)
+account_database.add_account(acc1)
+acc2 = Account("0001", "checking", "John Hennessy", 2000)
+account_database.add_account(acc2)
+acc3 = Account("0003", "saving", "Mark Hill", 3000)
+account_database.add_account(acc3)
+acc4 = Account("0004", "saving", "David Wood", 4000)
+account_database.add_account(acc4)
+acc5 = Account("0004", "saving", "David Wood", 4000)
+account_database.add_account(acc5)
+
+
 print(account_database)
-show_account('0003')
-deposit('0003', 50)
-show_account('0003')
-withdraw('0003', 25)
-show_account('0003')
-delete_account('0003')
-show_account('0003')
-deposit('0003', 50)
-withdraw('0001', 6000)
+print(account_database.search_acc_obj('0003'))
+account_database.search_acc_obj('0003').deposit(50)
+print(account_database.search_acc_obj('0003'))
+account_database.search_acc_obj('0003').withdraw( 25)
+print(account_database.search_acc_obj('0003'))
+# delete_account('0003')
+# show_account('0003')
+# deposit('0003', 50)
+# withdraw('0001', 6000)
